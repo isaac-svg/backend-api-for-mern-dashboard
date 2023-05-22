fe7f5c51dc59a925384cbabe8c7c35c30d386b96
isaac-svg <isaacsakyi009@gmail.com>
2023-05-22 21:13:23 +0000

    commit

--- routes/general.js
+++ routes/general.js
@@ -4,6 +4,6 @@ import { getUser, getDashboardStats } from "../controllers/general.js";
 const router = express.Router();
 
 router.get("/user/:id", getUser);
-router.route("/dashboard").get( getDashboardStats);
+router.get("/dashboard", getDashboardStats);
 
 export default router;
